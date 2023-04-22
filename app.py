7940b1f388656130df090f37b7b9be90f76c581c
samyoga <bhattaraisamyoga@gmail.com>
2023-04-22 13:10:24 -0500

    predict module

--- app.py
+++ app.py
@@ -4,6 +4,25 @@ from nltk.corpus import stopwords
 import re
 from nltk.stem.porter import PorterStemmer
 
+app = Flask(__name__)
+ps = PorterStemmer()
+
+model = pickle.load(open('model2.pkl', 'rb'))
+tfidfvect = pickle.load(open('tfidfvect2.pkl', 'rb'))
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+def predict(text):
+    review = re.sub('[^a-zA-Z]', ' ', text)
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    review_vect = tfidfvect.transform([review]).toarray()
+    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    return prediction
+
+if __name__ == "__main__":
+    app.run()
