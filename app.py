2bb65157421657bb7b59d86bdb084085bff3b511
samyoga <bhattaraisamyoga@gmail.com>
2023-04-22 19:42:51 -0500

    Modify predict method for model prediction

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from nltk.stem.porter import PorterStemmer
 app = Flask(__name__)
 ps = PorterStemmer()
 
-model = pickle.load(open('model2.pkl', 'rb'))
-tfidfvect = pickle.load(open('tfidfvect2.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+tfidfvect = pickle.load(open('tfidfvect.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,10 +18,19 @@ def predict(text):
     review = re.sub('[^a-zA-Z]', ' ', text)
     review = review.lower()
     review = review.split()
-    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    # review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
     review_vect = tfidfvect.transform([review]).toarray()
-    prediction = 'FAKE' if model.predict(review_vect) == 0 else 'REAL'
+    # for i in review_vect[0]:
+    #     if i == 0:
+    #         print('fake')
+    print(review_vect[0].size)
+    if model.predict(review_vect) == 0:
+        prediction = 'FAKE' 
+    elif model.predict(review_vect) ==1:
+        prediction = 'REAL'
+    else:
+        prediction = 'INVALID'
     return prediction
 
 @app.route('/', methods=['POST'])
